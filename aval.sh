a9e730e23bc9a1f4bbd23173d8a5953f9a16c043
Rodrigo Palermo <41303868+rodrigo-palermo@users.noreply.github.com>
2019-11-27 11:20:52 -0300

    Add files.

---
+++ aval.sh
@@ -0,0 +1,12 @@
+# Script 15: backup
+echo "Menu"
+echo "1- Criar pasta"
+echo "2- Criar arquivo"
+echo "3- Backup"
+echo "4- Abrir Calculadora"
+echo "5- Abrir Navegador"
+echo "6- Abrir link especifico no Navegador"
+echo "7- Listar/Terminar processo"
+echo -n "Escolha uma opcao: "; read opcao
+echo "Opcao escolhida: $opcao"
+# todo: switch case
